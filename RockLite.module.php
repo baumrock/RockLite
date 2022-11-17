6f63499c8ec5eeb0a752f13c4e0fc69b2c2e8c3f
BernhardBaumrock <office@baumrock.com>
2022-11-17 15:01:41 +0100

    add subscribe method

--- RockLite.module.php
+++ RockLite.module.php
@@ -9,12 +9,15 @@ namespace ProcessWire;
  */
 class RockLite extends WireData implements Module, ConfigurableModule
 {
+  public $http = false;
+  public $url;
+  public $apikey;
 
   public static function getModuleInfo()
   {
     return [
       'title' => 'RockLite',
-      'version' => '0.0.1',
+      'version' => '1.0.0',
       'summary' => 'MailerLite Integration',
       'autoload' => true,
       'singular' => true,
@@ -27,6 +30,49 @@ class RockLite extends WireData implements Module, ConfigurableModule
   public function init()
   {
     $this->url = "https://connect.mailerlite.com/";
+  }
+
+  /** ##### public api ##### */
+
+  public function get($url)
+  {
+    return json_decode(
+      $this->http()->get($this->url($url))
+    );
+  }
+
+  public function post($url, $data)
+  {
+    return json_decode(
+      $this->http()->post($this->url($url), json_encode($data))
+    );
+  }
+
+  public function subscribe($mail, $fields = [], $groups = [])
+  {
+    return $this->post("/api/subscribers", [
+      'email' => $mail,
+      'fields' => (object)$fields,
+      'groups' => $groups,
+    ]);
+  }
+
+  /** ##### internal ##### */
+
+  public function http(): WireHttp
+  {
+    if ($this->http) return $this->http;
+    /** @var WireHttp $http */
+    $http = $this->wire(new WireHttp());
+    $http->setHeader('Content-Type', 'application/json');
+    $http->setHeader('Accept', 'application/json');
+    $http->setHeader('Authorization', 'Bearer ' . $this->apikey);
+    return $this->http = $http;
+  }
+
+  public function url($url)
+  {
+    return $this->url . ltrim($url, "/");
   }
 
   /**
